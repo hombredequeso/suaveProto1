240df6d85c56fa389827750521f1783534d26e15
mcheeseman <mcheeseman@users.noreply.github.com>
2016-07-23 21:43:29 +1000

    Initial Commit

---
+++ suaveProto1/Program.fs
@@ -0,0 +1,17 @@
+﻿open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+
+[<EntryPoint>]
+let main argv = 
+    let app =
+        choose
+          [ GET >=> choose
+              [ path "/api/entity1" >=> OK "Hello entity1"
+                path "/api/entity2" >=> OK "Hello entity2" ]
+            POST >=> choose
+              [ path "/api/entity1" >=> OK "post entity1"
+                path "/api/entity2" >=> OK "post entity2" ] ]
+    startWebServer defaultConfig app
+    0
